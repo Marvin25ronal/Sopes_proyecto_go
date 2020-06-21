0e5dbdb3efd05df27fc384446d913730daadd5a1
Marvin Ronaldo Martinez Marroquin <marvin1ronal@gmail.com>
2020-06-21 01:25:15 +0000

    aplicacion

---
+++ src/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Bienvenido!!!!!!!")
+	fmt.Println("Ingrese las opciones")
+	fmt.Println("---------------------")
+	for {
+		fmt.Println("--------------------------------------------------Ingrese url del balanceador de carga-----------------------------------------")
+
+		url, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		url = strings.Replace(url, "\n", "", -1)
+		fmt.Println("Ingrese la cantidad de hilos---------------------------------")
+		hilos, _ := reader.ReadString('\n')
+		hilos = strings.Replace(hilos, "\n", "", -1)
+		fmt.Println("Ingrese la cantidad de solicitudes---------------------------")
+		solicitudes, _ := reader.ReadString('\n')
+		solicitudes = strings.Replace(solicitudes, "\n", "", -1)
+		fmt.Println("Ruta del archivo---------------------------------------------")
+		ruta, _ := reader.ReadString('\n')
+		ruta = strings.Replace(ruta, "\n", "", -1)
+		fmt.Println()
+		fmt.Println("Iniciando....................................................")
+		fmt.Println()
+		enviar(url, hilos, solicitudes, ruta)
+	}
+}
+func enviar(url string, hilos string, solicitudes string, archivo string) {
+	inthilos, err := strconv.Atoi(hilos)
+	intsolicitudes, err2 := strconv.Atoi(solicitudes)
+	if err != nil || err2 != nil {
+		fmt.Println("Error en los valores de solicitudes")
+		return
+	}
+	contenido, err3 := ioutil.ReadFile(archivo)
+	fmt.Println("Contenido ", string(contenido))
+	if err3 != nil {
+		fmt.Println("Error, no se pudo abrir el archivo")
+		return
+	}
+	txt := string(contenido)
+	for i := 0; i < inthilos; i++ {
+		fmt.Println("Hilo ", i)
+		go thread(url, intsolicitudes, txt)
+	}
+
+}
+
+func thread(url string, cantidad int, archivo string) {
+	for i := 0; i < cantidad; i++ {
+		fmt.Println("Solicitud ", i, url)
+		fmt.Println(archivo)
+	}
+	time.Sleep(120)
+}
